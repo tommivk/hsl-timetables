0c1157b999852d8704c1dd0a3718787f047369f9
tommivk <tommikokk@gmail.com>
2026-01-10 12:51:04 +0200

    add select input for schedules

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
+type Schedule = {
+  name: string;
+  src: string;
+};
+
 function App() {
+  const [schedules, _setSchedules] = useState<Array<Schedule>>([
+    {
+      name: "Rautatientori",
+      src: "https://omatnaytot.hsl.fi/view?cont=JqI+wR3diG+8Nubh3qIPiQ==",
+    },
+    {
+      name: "Korkeasaari",
+      src: "https://omatnaytot.hsl.fi/view?cont=dapkri+lgG88x5gc7YqcRg==",
+    },
+  ]);
+  const [scheduleSrc, setScheduleSrc] = useState<string>();
+
   return (
     <div className="container">
-      <iframe
-        className="schedule"
-        src="https://omatnaytot.hsl.fi/view?cont=JqI+wR3diG+8Nubh3qIPiQ=="
-      ></iframe>
+      <select onChange={(e) => setScheduleSrc(e.target.value)}>
+        {schedules.map(({ name, src }) => (
+          <option key={src} value={src}>
+            {name}
+          </option>
+        ))}
+      </select>
+      {scheduleSrc && <iframe className="schedule" src={scheduleSrc}></iframe>}
     </div>
   );
 }
