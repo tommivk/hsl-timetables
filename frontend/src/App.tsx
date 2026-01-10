589e7a1da812ff831a4e81f4f000baf7e10604ef
tommivk <tommikokk@gmail.com>
2026-01-10 12:25:17 +0200

    add iframe for schedule

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div>Hi</div>;
+  return (
+    <div className="container">
+      <iframe
+        className="schedule"
+        src="https://omatnaytot.hsl.fi/view?cont=JqI+wR3diG+8Nubh3qIPiQ=="
+      ></iframe>
+    </div>
+  );
 }
 
 export default App;
